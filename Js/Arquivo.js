65b64ff77b4797c7989a0b7075f656a842c159f9
Danrley <danrleysantos02@gmail.com>
2022-09-28 23:21:56 -0300

    melhoria ao precionar spaço para o pulo do mario

--- Js/Arquivo.js
+++ Js/Arquivo.js
@@ -1,23 +1,26 @@
-const mario = document.querySelector(".mario");
-const pipe = document.querySelector(".pipe");
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 const jump = () => {
-  mario.classList.add("jump");
+  mario.classList.add('jump');
   setTimeout(() => {
-    mario.classList.remove("jump");
+    mario.classList.remove('jump');
   }, 500);
 };
 
 const loop = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
+  const marioPosition = +window.getComputedStyle(mario) .bottom.replace('px', ' ');
 
   console.log(marioPosition);
 
-  if (pipePosition <= 120) {
-    pipe.style.animation = "none";
-    pipe.style.left = "${pipePosition}px";
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ) {
+    pipe.style.animation = 'none';
+    pipe.style.left = '${pipePosition}px';
+    mario.src = './Imagens/game-over.png';
+    mario.style.width ='75px'
+    mario.style.marginLeft = '50px'
+    clearInterval(loop);
   }
 }, 10);
-document.addEventListener("keydown", jump);
+document.addEventListener('keydown', jump);
+
